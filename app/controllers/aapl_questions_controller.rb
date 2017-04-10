a450e99db91b08a6d80ef503da8020b1a3c0dbd9
Raghu Betina <raghu@firstdraft.com>
2017-04-10 12:36:46 -0500

    Preliminary questions

--- app/controllers/aapl_questions_controller.rb
+++ app/controllers/aapl_questions_controller.rb
@@ -8,6 +8,37 @@ class AaplQuestionsController < ApplicationController
   def one
     # Your code goes below.
 
+    filename = Rails.root.join("lib", "csvs", "aapl.csv")
+    csv = CSV.open(filename, :headers => true, :encoding => "ISO-8859-1")
+
+    @total_quotes = 0
+    @closed_higher_than_open = 0
+    @closed_lower_than_open = 0
+    @declined = 0
+    @increased = 0
+
+    csv.each do |row|
+      hash = row.to_hash
+
+      @total_quotes = @total_quotes + 1
+
+      if hash["Close"] > hash["Open"]
+        @closed_higher_than_open = @closed_higher_than_open + 1
+      end
+
+      if hash["Close"] < hash["Open"]
+        @closed_lower_than_open = @closed_lower_than_open + 1
+      end
+
+      if hash["High"] <= hash["Open"]
+        @declined = @declined + 1
+      end
+
+      if hash["Low"] >= hash["Open"]
+        @increased = @increased + 1
+      end
+    end
+
     render("aapl_questions/one.html.erb")
   end
 
