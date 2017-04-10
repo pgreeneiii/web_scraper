db9e242d44c361009f46e90c8e854ff2c736d18f
Raghu Betina <raghu@firstdraft.com>
2017-04-10 12:08:07 -0500

    Add CSV parsing exercise scaffolding

---
+++ app/controllers/aapl_questions_controller.rb
@@ -0,0 +1,37 @@
+class AaplQuestionsController < ApplicationController
+  def home
+    # Your code goes below.
+
+    render("aapl_questions/home.html.erb")
+  end
+
+  def one
+    # Your code goes below.
+
+    render("aapl_questions/one.html.erb")
+  end
+
+  def two
+    # Your code goes below.
+
+    render("aapl_questions/two.html.erb")
+  end
+
+  def three
+    # Your code goes below.
+
+    render("aapl_questions/three.html.erb")
+  end
+
+  def four
+    # Your code goes below.
+
+    render("aapl_questions/four.html.erb")
+  end
+
+  def five
+    # Your code goes below.
+
+    render("aapl_questions/five.html.erb")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get("/", { :controller => "aapl_questions", :action => "home" })
+
+  get("/aapl/one", { :controller => "aapl_questions", :action => "one" })
+
+  get("/aapl/two", { :controller => "aapl_questions", :action => "two" })
+
+  get("/aapl/three", { :controller => "aapl_questions", :action => "three" })
+
+  get("/aapl/four", { :controller => "aapl_questions", :action => "four" })
+
+  get("/aapl/five", { :controller => "aapl_questions", :action => "five" })
 end
